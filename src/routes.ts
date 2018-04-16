52ca424787c36fa5aaf31950a8f7d3b9e713b5ac
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-17 03:31:12 +0700

    Simplify AppRoutes

--- src/routes.ts
+++ src/routes.ts
@@ -7,56 +7,26 @@ import * as Post from "./controller/Post";
  */
 export const AppRoutes = [
   // ROOT
-  {
-    path: "/",
-    method: "get",
-    action: Root.get
-  },
+  { path: "/", method: "get", action: Root.get },
+
   // TODOS
-  {
-    path: "/todos",
-    method: "get",
-    action: Todo.get
-  },
-  {
-    path: "/todos/:id",
-    method: "get",
-    action: Todo.getById
-  },
-  {
-    path: "/todos",
-    method: "post",
-    action: Todo.save
-  },
-  {
-    path: "/todos",
-    method: "delete",
-    action: Todo.destroy
-  },
-  {
-    path: "/todos/:id",
-    method: "delete",
-    action: Todo.destroyById
-  },
-  {
-    path: "/todos/:id",
-    method: "put",
-    action: Todo.updateById
-  },
+  { path: "/todos", method: "get", action: Todo.get },
+  { path: "/todos/:id", method: "get", action: Todo.getById },
+  { path: "/todos", method: "post", action: Todo.save },
+  { path: "/todos", method: "delete", action: Todo.destroy },
+  { path: "/todos/:id", method: "delete", action: Todo.destroyById },
+  { path: "/todos/:id", method: "put", action: Todo.updateById },
+
   // POSTS
-  {
-    path: "/posts",
-    method: "get",
-    action: Post.get
-  },
-  {
-    path: "/posts/:id",
-    method: "get",
-    action: Post.getById
-  },
-  {
-    path: "/posts",
-    method: "post",
-    action: Post.save
-  }
+  { path: "/posts", method: "get", action: Post.get },
+  { path: "/posts/:id", method: "get", action: Post.getById },
+  { path: "/posts", method: "post", action: Post.save },
+
+  // USERS
+  { path: "/users", method: "get", action: Todo.get },
+  { path: "/users/:id", method: "get", action: Todo.getById },
+  { path: "/users", method: "post", action: Todo.save },
+  { path: "/users", method: "delete", action: Todo.destroy },
+  { path: "/users/:id", method: "delete", action: Todo.destroyById },
+  { path: "/users/:id", method: "put", action: Todo.updateById }
 ];
