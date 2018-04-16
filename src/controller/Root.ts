426ee17bbf180bee765df03726c5c2a66a8ff2d0
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-17 06:25:38 +0700

    Start migrate to better project structure

--- src/controller/Root.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export async function get(req: Request, res: Response) {
-  res.status(200).send({
-    message: `Access these routes instead`,
-    routes: [`/todos`, `/posts`]
-  });
-}
--- src/entity/Category.ts
+++
@@ -1,8 +0,0 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-
-@Entity()
-export class Category extends BaseEntity {
-  @PrimaryGeneratedColumn() id: number;
-
-  @Column() name: string;
-}
