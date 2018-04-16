39e3c7334ab3e0016aaa76e724c6a259c220130d
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-17 03:37:16 +0700

    Initial TypeORM REST API project

---
+++ app/routes/SampleRoute.ts
@@ -0,0 +1,11 @@
+import * as express from "express";
+import { SampleController } from "../controllers/SampleController";
+import * as SampleMiddleware from "../middlewares/SampleMiddleware";
+
+export const SampleRoute: express.Router = express
+  .Router()
+  .get("/", SampleController.All)
+  .get("/:id", SampleController.Find)
+  .post("/", [SampleMiddleware.CheckCreate], SampleController.Create)
+  .put("/", [SampleMiddleware.CheckUpdate], SampleController.Update)
+  .delete("/", [SampleMiddleware.CheckDelete], SampleController.Delete);
